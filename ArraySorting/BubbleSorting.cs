c661cc30faaaa906c9d100c888a18661701c3bd7
Sergu <sergushov.1995@mail.ru>
2016-03-22 03:00:19 +0300

    stradegy factory, comparator class added

--- ArraySorting/BubbleSorting.cs
+++ ArraySorting/BubbleSorting.cs
@@ -6,145 +6,160 @@ using System.Threading.Tasks;
 
 namespace ArraySorting
 {
+    public enum Criterion{
+        ByMaxElem,
+        ByMinElem,
+        ByRowSum
+    };
     /// <summary>
     /// Предоставляет статические методы для сортировки массивов "пузырьковым" методом по различным критериям.
     /// </summary>
     public static class BubbleSorting
     {
-        /// <summary>
-        /// Сортирует исходный массив в порядке убывания минимальных элементов строк матрицы.
-        /// </summary>
-        /// <param name="arr">исходный массив</param>
-        public static void RowSortDescendingByMinElem(int[][] arr)
+        interface IStradegy
         {
-            for (int i = 0; i < arr.Length - 1; i++)
+            int GetKey(int[] arr);
+        }
+        class StradegyMaxElem : IStradegy
+        {
+            public int GetKey(int[] arr)
             {
-                for (int j = 0; j < arr.Length - i - 1; j++)
+                int max = int.MinValue;
+                for (int i = 0; i < arr.Length; i++)
                 {
-                    if (GetMinElement(arr[j]) < GetMinElement(arr[j + 1]))
+                    if (arr[i] > max)
                     {
-                        Swap(ref arr[j], ref arr[j + 1]);
+                        max = arr[i];
                     }
                 }
+                return max;
+            }
+        }
+        class StradegyMinElem : IStradegy
+        {
+            public int GetKey(int[] arr)
+            {
+                int min = int.MaxValue;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] < min)
+                    {
+                        min = arr[i];
+                    }
+                }
+                return min;
+            }
+        }
+        class StradegyRowSum : IStradegy
+        {
+            public int GetKey(int[] arr)
+            {
+                int sum = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+                return sum;
+            }
+        }
+        class Comparator : IComparer<int>
+        {
+            public int Compare(int numb1,int numb2)
+            {
+                if (numb1 == numb2)
+                {
+                    return 0;
+                }
+                else if (numb1 > numb2)
+                {
+                    return 1;
+                }
+                return -1;
             }
         }
         /// <summary>
-        /// Сортирует исходный массив в порядке возрастания минимальных элементов строк матрицы.
+        /// Сортирует исходный массив в порядке убывания в зависимости от передаваемого в метод критерия сравнения строк.
         /// </summary>
         /// <param name="arr">исходный массив</param>
-        public static void RowSortAscendingByMinElem(int[][] arr)
+        public static void SortDescending(int[][] arr, Criterion criterion)
         {
-            for (int i = 0; i < arr.Length - 1; i++)
+            IStradegy stradegy;
+            switch(criterion){
+                case Criterion.ByMaxElem:
+                    stradegy = new StradegyMaxElem();
+                    break;
+                case Criterion.ByMinElem:
+                    stradegy = new StradegyMinElem();
+                    break;
+                case Criterion.ByRowSum:
+                    stradegy = new StradegyRowSum();
+                    break;
+                default:
+                    stradegy = new StradegyRowSum();
+                    break;
+            };
+            SortDesc(arr, new Comparator(), stradegy);
+        }
+        /// <summary>
+        /// Сортирует исходный массив в порядке возрастания в зависимости от передаваемого в метод критерия сравнения строк.
+        /// </summary>
+        /// <param name="arr">исходный массив</param>
+
+        public static void SortAscending(int[][] arr, Criterion criterion)
+        {
+            IStradegy stradegy;
+            switch (criterion)
             {
-                for (int j = 0; j < arr.Length - i - 1; j++)
-                {
-                    if (GetMinElement(arr[j]) > GetMinElement(arr[j + 1]))
-                    {
-                        Swap(ref arr[j], ref arr[j + 1]);
-                    }
-                }
-            }
+                case Criterion.ByMaxElem:
+                    stradegy = new StradegyMaxElem();
+                    break;
+                case Criterion.ByMinElem:
+                    stradegy = new StradegyMinElem();
+                    break;
+                case Criterion.ByRowSum:
+                    stradegy = new StradegyRowSum();
+                    break;
+                default:
+                    stradegy = new StradegyRowSum();
+                    break;
+            };
+            SortAsc(arr, new Comparator(), stradegy);
         }
         /// <summary>
         /// Сортирует исходный массив в порядке возрастания максимальных элементов строк матрицы.
         /// </summary>
         /// <param name="arr">исходный массив</param>
-        public static void RowSortAscendingByMaxElem(int[][] arr)
+        
+        private static void SortAsc(int[][] arr,IComparer<int> comparator,IStradegy stradegy)
         {
             for (int i = 0; i < arr.Length - 1; i++)
             {
                 for (int j = 0; j < arr.Length - i - 1; j++)
                 {
-                    if (GetMaxElement(arr[j]) > GetMaxElement(arr[j + 1]))
+                    int firstKey = stradegy.GetKey(arr[j]);
+                    int secKey = stradegy.GetKey(arr[j + 1]);
+                    if (comparator.Compare(firstKey, secKey) == -1)
                     {
                         Swap(ref arr[j], ref arr[j + 1]);
                     }
                 }
             }
         }
-        /// <summary>
-        /// Cортирует исходный массив в порядке убывания максимальных элементов строк матрицы.
-        /// </summary>
-        /// <param name="arr">исходный массив</param>
-        public static void RowSortDescendingByMaxElem(int[][] arr)
+
+        private static void SortDesc(int[][] arr,IComparer<int> comparator,IStradegy stradegy)
         {
             for (int i = 0; i < arr.Length - 1; i++)
             {
                 for (int j = 0; j < arr.Length - i - 1; j++)
                 {
-                    if (GetMaxElement(arr[j]) < GetMaxElement(arr[j + 1]))
+                    int firstKey = stradegy.GetKey(arr[j]);
+                    int secKey = stradegy.GetKey(arr[j + 1]);
+                    if(comparator.Compare(firstKey,secKey) == 1)
                     {
                         Swap(ref arr[j], ref arr[j + 1]);
                     }
                 }
             }
-        }
-        /// <summary>
-        /// Сортирует исходный массив в порядке возрастания сумм элементов строк матрицы;
-        /// </summary>
-        /// <param name="arr">исходный массив</param>
-        public static void RowSortAscendingBySum(int[][] arr)
-        {
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                for (int j = 0; j < arr.Length - i - 1; j++)
-                {
-                    if (GetRowSum(arr[j]) > GetRowSum(arr[j + 1]))
-                    {
-                        Swap(ref arr[j], ref arr[j + 1]);
-                    }
-                }
-            }
-        }
-        /// <summary>
-        /// Сортирует исходный массив в порядке убывания сумм элементов строк матрицы;
-        /// </summary>
-        /// <param name="arr">исходный массив</param>
-        public static void RowSortDescendingBySum(int[][] arr)
-        {
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                for (int j = 0; j < arr.Length - i - 1; j++)
-                {
-                    if (GetRowSum(arr[j]) < GetRowSum(arr[j + 1]))
-                    {
-                        Swap(ref arr[j],ref arr[j+1]);
-                    }
-                }
-            }
-        }
-        private static int GetMaxElement(int[] arr)
-        {
-            int max = int.MinValue;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] > max)
-                {
-                    max = arr[i];
-                }
-            }
-            return max;
-        }
-        private static int GetMinElement(int[] arr)
-        {
-            int min = int.MaxValue;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] < min)
-                {
-                    min = arr[i];
-                }
-            }
-            return min;
-        }
-        private static int GetRowSum(int[] arr)
-        {
-            int sum = 0;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                sum += arr[i];
-            }
-            return sum;
         }
         private static void Swap(ref int[] firstRow,ref int[] secondRow)
         {
